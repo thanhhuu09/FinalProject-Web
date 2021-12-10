931818380a54329cd1b35f8efae498bcc03d99df
Sang.Sinh <73988351+thangtulaloinoi@users.noreply.github.com>
2021-12-10 15:27:47 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "310px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav(){
+    document.getElementById("mySidenav").style.width = "0px";
+}
+
+
+
