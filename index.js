ed43c4e0bb1afc6cd7fe173e38818500b7564700
Sang.Sinh <73988351+thangtulaloinoi@users.noreply.github.com>
2022-01-04 13:28:25 +0700

    Update

--- index.js
+++ index.js
@@ -8,5 +8,30 @@ function closeNav(){
     document.getElementById("mySidenav").style.width = "0px";
 }
 
+let check = false;
 
-
+document.addEventListener("click", (evt) => {
+    const flyoutElement = document.getElementById("user-info");
+    let targetElement = evt.target; // clicked element  
+    do {
+        if (targetElement == flyoutElement) {
+            // This is a click inside.
+            if (!check) {
+                check = true;
+                document.getElementsByClassName("navbar-user-list")[0].style.display = 'block';
+                return;
+            }
+            else if (check) {
+                check = false;
+                document.getElementsByClassName("navbar-user-list")[0].style.display = 'none';
+                return;
+            }
+        }
+        // Go up the DOM
+        targetElement = targetElement.parentNode;
+    } while (targetElement);
+    
+    // This is a click outside.
+    check = false;
+    document.getElementsByClassName("navbar-user-list")[0].style.display = 'none';
+});
